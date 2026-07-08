bfe9ea76dd376e5ff846781d7c967560d7ee4bd2
AmanyaPhillip <amanyaphillip@outlook.com>
2026-07-08 12:29:52 -0400

    feat: add lib/theme/app_colors.dart with LUXO brand color constants

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // Brand gold
+  static const Color gold = Color(0xFFD4AF37);
+  static const Color goldDark = Color(0xFFB8860B);
+
+  // Dark backgrounds
+  static const Color surfaceDark = Color(0xFF1A1A1A);
+  static const Color backgroundDeep = Color(0xFF0D0D0D);
+  static const Color inputFill = Color(0xFF2A2A2A);
+  static const Color borderIdle = Color(0xFF404040);
+
+  // Text
+  static const Color textLight = Color(0xFFE0E0E0);
+  static const Color textMuted = Color(0xFFB0B0B0);
+  static const Color textDisabled = Color(0xFF808080);
+}
